5aa9e68489056eb2cd5b4d32289f288aaafcdaef
dilshodbeksaidov96 <dilshodbeksaidov96@gmail.com>
2020-11-04 18:46:01 +0500

    getBounds bug fixed

--- app/src/main/java/java/com/semojies/MainActivity.java
+++ app/src/main/java/java/com/semojies/MainActivity.java
@@ -1,14 +1,22 @@
 package java.com.semojies;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import developer.semojis.Helper.EmojiconEditText;
+import developer.semojis.actions.EmojIconActions;
 
 public class MainActivity extends AppCompatActivity {
+    EmojIconActions emojIconActions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        emojIconActions = new EmojIconActions(getApplicationContext(), getWindow().getDecorView().findViewById(android.R.id.content), (EmojiconEditText) findViewById(R.id.edt), (ImageView) findViewById(R.id.btn));
+        emojIconActions.addEmojiconEditTextList((EmojiconEditText) findViewById(R.id.edt));
+        emojIconActions.ShowEmojIcon();
     }
 }
